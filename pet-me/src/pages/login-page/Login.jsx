80db530b5e3d27a143005e8735b991ea893f41df
osman122 <osoconan1@gmail.com.com>
2023-11-05 17:51:42 +0200

    mm

--- pet-me/src/pages/login-page/Login.jsx
+++ pet-me/src/pages/login-page/Login.jsx
@@ -1,9 +1,9 @@
-const Login = () => {
-    return ( 
-        <div>
-            <h1>Login Page</h1>
-        </div>
-     );
+export default function Login () {
+    
+    return (<div id="login">
+   <div class="alert alert-secondary d-flex justify-content-between align-items-center" role="alert">
+       log in her
+       
+   </div>
+</div>)
 }
- 
-export default Login;
