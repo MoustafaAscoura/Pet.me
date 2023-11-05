bd6b2422c97f8970a6f8a9bd17d38d6a403c514c
mohamedqadryahmed <mohamedqadryahmed@gmail.com>
2023-11-05 18:40:29 +0200

    fix header

--- pet-me/src/App.js
+++ pet-me/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/Router";
 import Header from "./components/header/Header";
+import './app.css'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div className="container my-5">
+      <div className="container main">
         <Router />
       </div>
     </BrowserRouter>
--- pet-me/src/components/header/Header.jsx
+++ pet-me/src/components/header/Header.jsx
@@ -2,18 +2,20 @@ import { faComments } from "@fortawesome/free-regular-svg-icons";
 import {faMagnifyingGlass,faPaw} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg fixed-top">
       <div className="container">
-        <div
+        <Link
+          to='/'
           className="navbar-brand me-auto fw-bold"
           style={{ color: "#8c594d" }}
         >
           <FontAwesomeIcon icon={faPaw} className="me-2" />
           Pet.me
-        </div>
+        </Link>
         <div
           className="offcanvas offcanvas-end"
           tabindex="-1"
@@ -21,9 +23,14 @@ function Header() {
           aria-labelledby="offcanvasNavbarLabel"
         >
           <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-              Pet.me
-            </h5>
+          <Link
+          to='/'
+          className="navbar-brand me-auto fw-bold"
+          style={{ color: "#8c594d" }}
+        >
+          <FontAwesomeIcon icon={faPaw} className="me-2" />
+          Pet.me
+        </Link>
             <button
               type="button"
               className="btn-close"
@@ -34,30 +41,30 @@ function Header() {
           <div className="offcanvas-body">
             <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
               <li className="nav-item">
-                <a
-                  className="nav-link mx-lg-2 active"
+                <Link
+                  className="nav-link mx-lg-2"
                   aria-current="page"
-                  href="#"
+                  to="/"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link mx-lg-2" href="#">
+                <Link className="nav-link mx-lg-2" to="/explore">
                   Explore
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link mx-lg-2" href="#">
+                <Link className="nav-link mx-lg-2" to="/about">
                   About
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <form class="input-group">
+                <form class="input-group mx-lg-2">
                   <input
                     type="text"
                     placeholder="Search here ..."
-                    class="form-control"
+                    className="form-control"
                     id="inputGroupFile04"
                   />
                   <button
@@ -69,21 +76,25 @@ function Header() {
                   </button>
                 </form>
               </li>
-              <li className="nav-item">
-                <a className="nav-link mx-lg-2 fs-4" href="#">
+              <li className="nav-item ms-3">
+                <Link className="nav-link mx-lg-2 fs-4" to="/chat">
                   <FontAwesomeIcon
                     icon={faComments}
                     style={{ color: "#8c594d" }}
                   />
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
 
-        <a href="#" className="login-button">
+        <Link to="/login" className="login-button mx-lg-2">
           Login
-        </a>
+        </Link>
+
+        <Link to="/signup" className="ms-3 login-button">
+          Signup
+        </Link>
         <button
           className="navbar-toggler pe-0"
           type="button"
