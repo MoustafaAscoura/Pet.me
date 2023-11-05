406e20229053bf4eabcc4185eca7a5a223eee985
mariamtamerr <memariam5@outlook.com>
2023-11-05 18:07:57 +0200

    Added Postgresql pet_me Database

--- django_/django_/settings.py
+++ django_/django_/settings.py
@@ -175,12 +175,15 @@ WSGI_APPLICATION = 'django_.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default": {
+"ENGINE": "django.db.backends.postgresql",
+"NAME": "pet_me",
+"USER": "mariam",
+"PASSWORD": "123456789@m",
+"HOST": "localhost",
+"PORT": "5432",
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
