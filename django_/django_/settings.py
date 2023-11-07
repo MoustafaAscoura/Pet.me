7f0762fe07d360d30a397366c9cfa1e363adcac8
mariamtamerr <memariam5@outlook.com>
2023-11-07 20:37:27 +0200

    Users Endpoint Through Post Method

--- django_/django_/settings.py
+++ django_/django_/settings.py
@@ -103,34 +103,35 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "UPDATE_LAST_LOGIN": True,
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    # "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
 }
 
 # Djoser Settings
-# DJOSER = {
-#     'LOGIN_FIELD': 'email',
-#     'USER_CREATE_PASSWORD_RETYPE':True,
-#     'ACTIVATION_URL':'/activate/{uid}/{token}',
-#     'SEND_ACTIVATION_EMAIL':True,
-#     'SEND_CONFIRMATION_EMAIL':True,
-#     'PASSWORD_CHANGED_EMAIL_CONFIRMATION':True,
-#     'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}',
-#     'SET_PASSWORD_RETYPE': True,
-#     'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
-#     "ACTIVATION_URL": "activate/{uid}/{token}",
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'USER_CREATE_PASSWORD_RETYPE':True,
+    'ACTIVATION_URL':'/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL':True,
+    'SEND_CONFIRMATION_EMAIL':True,
+    'PASSWORD_CHANGED_EMAIL_CONFIRMATION':True,
+    'PASSWORD_RESET_CONFIRM_URL': 'password-reset/{uid}/{token}',
+    'SET_PASSWORD_RETYPE': True,
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    'TOKEN_MODEL':None, #To delete user lazm a-set it to none
+    "ACTIVATION_URL": "activate/{uid}/{token}",
 #     "SEND_ACTIVATION_EMAIL": True,
 #     "SOCIAL_AUTH_TOKEN_STRATEGY": "djoser.social.token.jwt.TokenStrategy",
 #     # "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": [
 #     #     "your redirect url",
 #     #     "your redirect url",
 #     # ],
-#     "SERIALIZERS": {
-#         "user_create": "accounts.serializers.UserCreateSerializer",  # custom serializer
-#         "user": "djoser.serializers.UserSerializer",
-#         "current_user": "djoser.serializers.UserSerializer",
-#         "user_delete": "djoser.serializers.UserSerializer",
-#     },
-# }
+    "SERIALIZERS": {
+        "user_create": "accounts.serializers.UserCreateSerializer",  # custom serializer
+        "user": "djoser.serializers.UserSerializer",
+        "current_user": "djoser.serializers.UserSerializer",
+        "user_delete": "djoser.serializers.UserSerializer",
+    },
+}
 
 
 
@@ -139,10 +140,10 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Email Configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "localhost"
+# EMAIL_HOST = "localhost"
 EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = "1025"
-# EMAIL_PORT = "587"
+EMAIL_PORT = "587"
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
