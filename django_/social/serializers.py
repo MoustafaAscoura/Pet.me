086b055bd2ac6b5d0d9efbbf865b379a94562999
mariamtamerr <memariam5@outlook.com>
2023-11-09 02:56:43 +0200

    Added extra fields for other seriaizers

--- django_/social/serializers.py
+++ django_/social/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Post,Comment, Reply, Message, Report
 
+
+
 class PostsSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField()
 
@@ -12,31 +14,61 @@ class PostsSerializer(serializers.ModelSerializer):
         return obj.user.username
 
 
+
+
 class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = Comment 
-        fields = '__all__'
+        fields = ['id', 'user', 'post', 'content', 'created_at', 'username']
+
+    def get_username(self, obj):
+        return obj.user.username
+
+
+
+
 
 
 class ReplySerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = Reply 
-        fields = '__all__'
+        fields = ['id', 'user', 'comment', 'content', 'created_at', 'username']
+
+    def get_username(self, obj):
+        return obj.user.username
 
 
-    
+
+
+
 class MessageSerializer(serializers.ModelSerializer):
+    sender_username = serializers.SerializerMethodField()
+    receiver_username = serializers.SerializerMethodField()
+
     class Meta:
         model = Message 
-        fields = '__all__'
+        fields = ['id', 'sender', 'receiver', 'content', 'created_at', 'sender_username', 'receiver_username']
+
+    def get_sender_username(self, obj):
+        return obj.sender.username
+
+    def get_receiver_username(self, obj):
+        return obj.receiver.username
 
 
     
 
+    
 class ReportsSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
     class Meta:
         model = Report
-        fields = '__all__'
-        #set depth = 1 user details
+        fields = ['id', 'user', 'post', 'created_at', 'comment', 'reason', 'username']
 
-
+    def get_username(self, obj):
+        return obj.user.username
