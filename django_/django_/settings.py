a7c7950a021bd9b6ad080c9c436dd4593d5cb482
moustafa ascoura <moustafaascoura@gmail.com>
2023-11-13 14:46:47 +0200

    CORS Whitelist

--- django_/django_/settings.py
+++ django_/django_/settings.py
@@ -77,6 +77,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 ROOT_URLCONF = 'djoserauthapi.urls'
 
