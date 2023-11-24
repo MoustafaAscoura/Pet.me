b4f3be0afc3c2dbed9742b4e1f1813cf17c5cbb5
Osman122 <osoconan1@gmail.com>
2023-11-24 04:08:13 +0200

    add env for live chat using socket

---
+++ django_/chats/routing.py
@@ -0,0 +1,15 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+# import app.routing
+from django.urls import re_path
+from app.consumers import TextRoomConsumer
+websocket_urlpatterns = [
+    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+]
+# the websocket will open at 127.0.0.1:8000/ws/<room_name>
+application = ProtocolTypeRouter({
+    'websocket':
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ,
+})
