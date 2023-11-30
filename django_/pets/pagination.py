4e6ebd71e71be749da0f24366b0613967b72aad7
moustafa ascoura <moustafaascoura@gmail.com>
2023-11-30 13:33:41 +0200

    pagination edits

--- django_/pets/pagination.py
+++ django_/pets/pagination.py
@@ -2,6 +2,7 @@ from rest_framework import pagination
 from rest_framework.response import Response
 
 class CustomPagination(pagination.PageNumberPagination):
+    page_size = 12
 
     def get_paginated_response(self, data):
         return Response({
